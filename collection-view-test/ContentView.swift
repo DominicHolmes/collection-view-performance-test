9830c0de36d6a6870cf7da571708f3f816d19231
dominic <hello@dom.voyage>
2019-06-14 19:03:44 -0500

    Initial Commit

---
+++ collection-view-test/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  collection-view-test
+//
+//  Created by Dominic Holmes on 6/14/19.
+//  Copyright © 2019 Dominic Holmes. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
