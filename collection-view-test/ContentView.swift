50f25b2e3989263fb7120c4c822b32f5eb443f9d
dominic <hello@dom.voyage>
2019-06-15 16:14:00 -0500

    completed test

--- collection-view-test/ContentView.swift
+++ collection-view-test/ContentView.swift
@@ -8,9 +8,54 @@
 
 import SwiftUI
 
+struct Box: Identifiable {
+    let id: Int
+    let name: String
+}
+
 struct ContentView : View {
+    let boxes: [Box] = {
+        var temp = [Box]()
+        for i in 0 ... 10000 {
+            temp.append(Box(id: i, name: "Box \(i)"))
+        }
+        return temp
+    }()
+    
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            VStack {
+                ScrollView {
+                    HStack {
+                        ForEach(boxes) { box in
+                            VStack {
+                                Image("tweaks")
+                                    .resizable()
+                                    .cornerRadius(12)
+                                    .frame(width: 100, height: 100)
+                                Text(box.name)
+                                    .font(.subheadline)
+                                    .fontWeight(.bold)
+                            }
+                        }
+                    }
+                }
+                .padding(20)
+                .navigationBarTitle(Text("Collection Test"))
+                
+                List(boxes) { box in
+                    VStack {
+                        Image("tweaks")
+                            .resizable()
+                            .cornerRadius(12)
+                            .frame(width: 100, height: 100)
+                        Text(box.name)
+                            .font(.subheadline)
+                            .fontWeight(.bold)
+                    }
+                }
+            }
+        }
     }
 }
 
